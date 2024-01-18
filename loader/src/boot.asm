e4785b330bbd03ae7ce81005c660de39518f6eb5
Will Hopkins <willothyh@gmail.com>
2024-01-18 14:10:28 -0800

    small changes

--- loader/src/boot.asm
+++ loader/src/boot.asm
@@ -8,23 +8,6 @@ Comments in this codebase are a mix of copy-paste from the above sources, and my
 */
 
 .code32
-/*
-The multiboot standard does not define the value of the stack pointer register
-(esp) and it is up to the kernel to provide a stack. This allocates room for a
-small stack by creating a symbol at the bottom of it, then allocating 16384
-bytes for it, and finally creating a symbol at the top. The stack grows
-downwards on x86. The stack is in its own section so it can be marked nobits,
-which means the kernel file is smaller because it does not contain an
-uninitialized stack. The stack on x86 must be 16-byte aligned according to the
-System V ABI standard and de-facto extensions. The compiler will assume the
-stack is properly aligned and failure to align the stack will result in
-undefined behavior.
-*/
-.section .bss
-.align 16
-stack_bottom:
- .skip 16384 /* 16 KiB */
-stack_top:
 
 /*
 The linker script specifies _start as the entry point to the kernel and the
@@ -46,19 +29,19 @@ _start:
 	itself. It has absolute and complete power over the
 	machine.
 	*/
-  cli
-
 
 	/*
 	To set up a stack, we set the esp register to point to the top of the
 	stack (as it grows downwards on x86 systems). This is necessarily done
 	in assembly as languages such as C cannot function without a stack.
 	*/
-	mov esp, 0x7c00
-  mov ebp, 0x7c00
+  mov esp, 0x7c00
+  mov ebp, esp
 
   push ebx
   call loader_main
+
+  cli
 
   jmp end
 
